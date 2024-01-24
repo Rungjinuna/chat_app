b4a1bc76bc9fae1d64b9ee44d9aab3c7ed5cbcb8
Eunsol <rungjinuna@naver.com>
2024-01-24 13:02:11 +0900

    new message api

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -6,14 +6,20 @@ import prisma from '@/libs/prismadb';
 
 export async function POST(request: Request) {
   try {
+    // getCurrentUser함수 호출하여 현재 로그인한 사용자 확인
     const currentUser = await getCurrentUser();
+    // 요청 데이터 처리
+    // request.json() 메소드를 사용해 클라이언트로부터 전송된 데이터 추출
+    // message, image, conversationId 추출
     const body = await request.json();
     const { message, image, conversationId } = body;
-
+    // 유효한 사용자가 아니면 401 반환
     if (!currentUser?.id || !currentUser?.email) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
-
+    // 새 메시지 생성
+    // prisma.message.create 메소드를 사용하여 새 메시지를 데이터베이스에 저장함
+    // 메세지 데이터는 본문, 이미지, conversationId, currentUser.id 포함
     const newMessage = await prisma.message.create({
       include: {
         seen: true,
@@ -35,7 +41,9 @@ export async function POST(request: Request) {
         },
       },
     });
-
+    // 대화 업데이트
+    // prisma.conversation.update 메소드를 사용하여 대화를 업데이트함
+    // 마지막 메세지의 시간을 현재시간으로 설정하고 새로 생성된 메세지를 대화에 연결함
     const updatedConversation = await prisma.conversation.update({
       where: {
         id: conversationId,
@@ -57,19 +65,30 @@ export async function POST(request: Request) {
         },
       },
     });
-
+    // pusherServer.trigger메소드를 사용해서 conversationId채널에 message:new 이벤트 전송
+    // 상대방의 대화창에 새로운 메세지를 보여주기위해서
     await pusherServer.trigger(conversationId, 'messages:new', newMessage);
-
+    // 마지막 메세지 추출
+    // updatedConversation.messages 이 배열은 updatedConversation객체 내에 있는 메세지들 포함
+    // updatedConversation은 대화에 대한 최신정보를 담고있으며, 여기에는 대화의 모든 메세지가 포함됨
+    // updatedConversation.messages.length-1 :배열의 길이에서 1을 빼면 마지막 요소의 인덱스가 반환됨
     const lastMessage =
       updatedConversation.messages[updatedConversation.messages.length - 1];
+    //즉 배열의 마지막 요소를 나타내며 가장 최근에 추가된 메세지를 의미함
 
+    //대화에 참여한 모든 사용자를 순회하면서
+    // pusherServer객체의 trigger 메소드를 사용하여 pusher서버에 실시간 이벤트를 트리거함
     updatedConversation.users.map((user) => {
+      // user,email은 pusher채널의 이름으로 사용됨
+      //  conversation:update 트리거할 이벤트 이름
+      // 여기서 Id와 messages(메세지배열, 여기서는 최근 메세지만 포함)를 내보냅니다.
+      // 단언 연산자를 사용하여 user.email이 undefined 혹은 null이 아님을 단언함
       pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
         messages: [lastMessage],
       });
     });
-
+    // 성공적으로 메세지를 처리한 후 newMessage객체를 Json형식으로 변환하여 응답함.
     return NextResponse.json(newMessage);
   } catch (error) {
     console.log(error, 'ERROR_MESSAGES');
--- src/libs/pusher.ts
+++ src/libs/pusher.ts
@@ -1,6 +1,12 @@
 import PusherServer from 'pusher';
 import PusherClient from 'pusher-js';
 
+// 서버측 푸셔 인스턴스생성
+// 메세지를 푸시하고 채널을 관리하는데 사용됨
+// 환경변수 : appId, key, secret등은 환경 변수에서 가져옴.
+// 이들은 푸셔 서비스를 사용하기 위한 인증 정보임
+// cluster는 푸셔 서비스의 클러스터를 지정함
+// useTLS : TLS 전송계층보안을 사용하여 보안 통신 활성화
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
@@ -9,11 +15,15 @@ export const pusherServer = new PusherServer({
   useTLS: true,
 });
 
+// 클라이언트측 푸셔 인스턴스 생성, 실시간으로 데이터를 받기 위해 서버와 통신함
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
+    // channelAuthorization 푸셔 채널에 접근하기 위한 인증 엔드포인트를 저장함
+    // 여기서 api.pusher/auth는 클라이언트가 채널에 접근할 때 인증을 처리하는 서버측 경로
     channelAuthorization: {
       endpoint: '/api/pusher/auth',
+      // 인증방식으로 ajax를 사용함
       transport: 'ajax',
     },
     cluster: 'ap3',
